ca32cab8edb2bdab1ebd52caf6507ccb3ea8a946
Nikhil Khedekar <nikhilvkhedekar@gmail.com>
2022-03-09 17:48:29 +0100

    Updated license

--- include/device_event_handler.h
+++ include/device_event_handler.h
@@ -1,3 +1,9 @@
+// Copyright (c) 2022, Autonomous Robots Lab, Norwegian University of Science and Technology
+// All rights reserved.
+
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree. 
+
 #ifndef DEV_EVENT_HANDLER_
 #define DEV_EVENT_HANDLER_
 #include "Spinnaker.h"
--- include/image_event_handler.h
+++ include/image_event_handler.h
@@ -1,3 +1,9 @@
+// Copyright (c) 2022, Autonomous Robots Lab, Norwegian University of Science and Technology
+// All rights reserved.
+
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree. 
+
 #ifndef IMG_EVENT_HANDLER_
 #define IMG_EVENT_HANDLER_
 #include <sensor_msgs/image_encodings.h>
