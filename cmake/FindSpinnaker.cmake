dfdbce861276081d212ddd29e700444e98d1d77d
Nikhil Khedekar <nikhilvkhedekar@gmail.com>
2022-03-09 17:46:33 +0100

    Updated license

--- cmake/FindSpinnaker.cmake
+++ cmake/FindSpinnaker.cmake
@@ -1,3 +1,9 @@
+# Copyright (c) 2022, Autonomous Robots Lab, Norwegian University of Science and Technology
+# All rights reserved.
+
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. 
+
 unset(Spinnaker_FOUND)
 unset(Spinnaker_INCLUDE_DIRS)
 unset(Spinnaker_LIBRARIES)
