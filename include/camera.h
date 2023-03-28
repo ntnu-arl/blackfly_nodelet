bc9fcc0ab2b83bd0710f1f3b5727a51595f8dbac
morten <morten.c.nissov@gmail.com>
2023-03-28 16:51:24 +0200

    del param in destructor

--- include/camera.h
+++ include/camera.h
@@ -144,6 +144,8 @@ public:
       m_cam_ptr->DeInit();
       std::free(user_buffer);
     }
+
+    ros::param::del(ros::this_node::getName() + node_ready_param_name_);
   }
   // This function sets the internal buffersize of spinnaker -> By default it allocates memory based
   // on the frame rate of the camera. This may require very large memory when using multiple
